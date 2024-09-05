6f724fdf4c56eb222c2aa5e5652af061e6af077e
d34d <84479990+Vajean1@users.noreply.github.com>
2024-09-05 16:05:10 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,16 +1,1 @@
-from baixar_music import baixar_musica, pesquisar_video
-
-def PegarMusica(string=str):
-    if len(string) > 0:
-        if '!musica' in string:
-            if not string == '!musica':
-                string = string.split('!musica')
-                return ''.join(string).strip()
-            else:
-                print('Digite algo após o comando..')
-                return False
-        else:
-            return False
-
-print(PegarMusica('!musica bury me alive'))
-baixar_musica(pesquisar_video(PegarMusica('!musica bury me alive')))
+#Área de testes
